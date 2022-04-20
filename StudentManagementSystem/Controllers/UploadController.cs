8910ffc5c7374bb467274799f74d00886780e13d
PMS <raviecranjan@gmail.com>
2022-04-20 10:17:42 +0530

    Upload_Blob_File

---
+++ StudentManagementSystem/Controllers/UploadController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.Infrastructure;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class UploadController : Controller
+    {
+        public UploadFile _uploadFileToBlob;
+        public UploadController(UploadFile uploadFileToBlob)
+        {
+            _uploadFileToBlob = uploadFileToBlob;
+        }
+
+        [Route("Upload/UploadFileAsync")]
+        public IActionResult UploadFileAsync()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UploadFileAsync1()
+        {
+            try
+            {
+                var formCollection = await Request.ReadFormAsync();
+                var file = formCollection.Files.First();
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    string fileURL = await _uploadFileToBlob.UploadFileToBlobAsync(file.OpenReadStream(), fileName, file.ContentType);
+                    ViewBag.Message = string.Format("File Uploaded Successfully");
+                    return View("UploadFile");
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+}
---
+++ StudentManagementSystem/Infrastructure/UploadFile.cs
@@ -0,0 +1,51 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem.Infrastructure
+{
+    public class UploadFile
+    {
+        private readonly ILogger _logger;
+
+        public IConfiguration _configuration;
+
+        public BlobServiceClient _client;
+
+        public BlobContainerClient _containerClient;
+        private readonly string _blobConnectionString;
+        private readonly string _containerName;
+        public UploadFile(IConfiguration _configuration,
+            ILogger<SendServiceBusMessage> logger)
+        {
+            _logger = logger;
+            _blobConnectionString = _configuration["blobConnectionString"];
+            _containerName = _configuration["containerName"];
+        }
+
+        public async Task<string> UploadFileToBlobAsync(Stream fileStream, string fileName, string contentType)
+        {
+            try
+            {
+                var container = new BlobContainerClient(_blobConnectionString, _containerName);
+                var createResponse = await container.CreateIfNotExistsAsync();
+                if (createResponse != null && createResponse.GetRawResponse().Status == 201)
+                    await container.SetAccessPolicyAsync(PublicAccessType.Blob);
+                var blob = container.GetBlobClient(fileName);
+                await blob.DeleteIfExistsAsync(DeleteSnapshotsOption.IncludeSnapshots);
+                await blob.UploadAsync(fileStream, new BlobHttpHeaders { ContentType = contentType });
+                return blob.Uri.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
